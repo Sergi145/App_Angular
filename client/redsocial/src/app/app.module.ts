75246e798b1b4a452fd18c59a7d3276acdc4fe32
sergi <chechi14_1986@hotmail.com>
2017-12-18 23:56:43 +0100

    registro de usuarios en la plataforma

--- client/redsocial/src/app/app.module.ts
+++ client/redsocial/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+//import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 import {routing,appRoutingProviders} from './app.routing';
 
 
@@ -19,7 +20,8 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    routing,
+    HttpClientModule
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
--- client/redsocial/src/app/components/register/register.component.ts
+++ client/redsocial/src/app/components/register/register.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Router,ActivatedRoute,Params} from '@angular/router';
 import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers:[UserService]//aqui introducimos los servicios que necesitamos
 })
 export class RegisterComponent implements OnInit {
 
-	public user:User
+	public user:User;
+  public status:string;
 
   constructor(
 
   	private _route:ActivatedRoute,
-  	private _router:Router
+  	private _router:Router,
+    private _userService:UserService//injectamos el servicio para poder utilizarlo
 
   	) {
 
@@ -23,6 +27,32 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+
+    this._userService.register(this.user).subscribe(
+
+       response=>{
+
+         if(response.user && response.user._id){
+
+           console.log(response.user);
+
+             this.status='success';
+             form.reset();
+         }
+         else{
+
+            this.status='error';
+         }
+
+       },
+       error=>{
+         console.log(<any>error);
+       }
+
+      );
   }
 
 }
